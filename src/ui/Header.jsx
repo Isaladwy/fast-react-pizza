22e1bb47e457226306fda12c3e845ad7feaf97d2
Isaladwy <isaladwy@outlook.com>
2024-07-17 16:14:34 +0300

    Fetching Orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
-      <Link to='/'>Fast React Pizza Co.</Link>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
 
       <p>Tamim</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
