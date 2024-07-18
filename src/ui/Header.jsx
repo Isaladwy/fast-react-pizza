31182ca3a6d8fc079a7cb14b734f19c9ceb8701f
Isaladwy <isaladwy@outlook.com>
2024-07-18 23:04:01 +0300

    Box model spacing

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,13 +4,13 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="bg-yellow-500 px-4 py-3 uppercase border-stone-200 border-b">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
 
-      <Username/>
+      <Username />
     </header>
   );
 }
